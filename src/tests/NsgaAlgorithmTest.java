7bd797c334ca0f4aa615be51c84057681036e20b
Szuhanyi <gyurka_szuhanyi@yahoo.com>
2019-07-27 11:21:42 +0300

    additional test

--- src/tests/NsgaAlgorithmTest.java
+++ src/tests/NsgaAlgorithmTest.java
@@ -1,5 +1,36 @@
+package tests;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+
+// so this is jUnit 4, or 5//
+// should I look after mockito as well ? ???
+
+
 
 class NsgaAlgorithmTest {
 
+    @org.junit.jupiter.api.Test
+    void nonDominatedSort() {
+    // should I create a case manually ? and then implement it here ?
+        // what not is really
+
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRanking() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRankedList() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFirstFront() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFrontCount() {
+    }
 }
